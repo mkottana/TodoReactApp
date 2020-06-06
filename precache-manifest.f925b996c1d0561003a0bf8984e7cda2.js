43b1de15529ca7c0dcd7fd79d4d11f41451ee5d5
mkottana <48954734+mkottana@users.noreply.github.com>
2020-06-06 23:29:48 +0530

    Updates

---
+++ precache-manifest.f925b996c1d0561003a0bf8984e7cda2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "379c6c72c76937d9b8a3584786d6bb26",
+    "url": "/TodoReactApp/index.html"
+  },
+  {
+    "revision": "21e2a2d765965c9c3478",
+    "url": "/TodoReactApp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a2d09780f355acd8cfe2",
+    "url": "/TodoReactApp/static/js/2.49a6b46f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TodoReactApp/static/js/2.49a6b46f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21e2a2d765965c9c3478",
+    "url": "/TodoReactApp/static/js/main.94feb708.chunk.js"
+  },
+  {
+    "revision": "eccb6c85de55f9be53fa",
+    "url": "/TodoReactApp/static/js/runtime-main.91b7a5b1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/TodoReactApp/static/media/logo.5d5d9eef.svg"
+  }
+]);
